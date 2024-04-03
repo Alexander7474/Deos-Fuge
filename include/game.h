31d9608062125ada370f3acc11999a1adde44a10
Alexander7474 <alexandre.lanternier@outlook.fr>
2024-04-03 19:52:03 +0200

    modif game

---
+++ include/game.h
@@ -0,0 +1,62 @@
+#pragma once 
+
+////////////////////
+/// External headers
+////////////////////
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <BBOP/Graphics.h>
+#include <vector>
+
+/////////////////
+/// Local headers
+/////////////////
+#include "map.h"
+#include "personnage.h"
+#include "projectiles.h"
+
+enum game_state_enum { Start=0, Middle=1, End=2};
+
+/**
+ * @class Game
+ * @brief class qui gère la partie
+ * 
+ * @note La class n'hérite pas de BbopDrawable car elle gère sa propre Scene
+ */
+class Game
+{
+public:
+  /**
+  * @brief Constructeur de Game
+  *
+  * @param[in] map_ Map à utiliser pour la game 
+  * @param[in] nPersonnage_ nombre de personnage 
+  * @param[in] nBot_ nombre de bot
+  *
+  * @see Map Personnage
+  */
+  Game(Map map_, int n_personnage_, int n_bot_);
+
+  /**
+  * @brief Met a jour tous les attribut de la Game
+  *
+  * @note À utiliser à chaque frame
+  */
+  void update();
+
+  /**
+  * @brief Dessine tous les attribut de la game 
+  *
+  * @param[in] window fenêtre glfw initialisé avec bbopInit
+  *
+  * @note À utiliser à chaque frame avec 
+  */
+  void Draw(GLFWwindow *&window) const;
+private:
+  game_state_enum game_state; //<! statut de la game
+  Map map; //<! Map où se déroule la game
+  int n_personnage; //<! nombre de personnage
+  int n_bot; //<! nombre de Bot 
+  std::vector<Personnage> personnages; //<! liste des personnages
+  std::vector<Projectile> projectiles; //<! liste des projectiles
+};
