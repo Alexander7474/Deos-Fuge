1afa65a5f7160d9159e49c8cad28ad54632c42dc
Alexander7474 <alexandre.lanetrnier@outlook.fr>
2024-06-10 00:50:05 +0200

    intégration des nouvelles animations

--- src/perso_info.cpp
+++ src/perso_info.cpp
@@ -3,7 +3,7 @@
 // perso par default
 const perso_info DEFAULT_PERSO = {
   "img/personnages/default/",
-  {3,4,3,3,3,3,3}, // stationnary|run|dash|jump|fall|light_attack|hit
+  {1,6,3,5,1,3,3}, // stationnary|run|dash|jump|fall|light_attack|hit
   5.f,
   75.f
 };
