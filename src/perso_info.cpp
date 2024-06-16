39f96da60059ee6a9b072065645210ff95e59885
Alexander7474 <alexandre.lanetrnier@outlook.fr>
2024-06-16 19:03:02 +0200

    integration des sprites

--- src/perso_info.cpp
+++ src/perso_info.cpp
@@ -3,7 +3,7 @@
 // perso par default
 const perso_info DEFAULT_PERSO = {
   "img/personnages/default/",
-  {1,6,3,4,4,8,3}, // stationnary|run|dash|jump|fall|light_attack|hit
+  {20,6,1,4,4,5,4}, // stationnary|run|dash|jump|fall|light_attack|hit
   5.f,
   75.f
 };
